063d6c6b4e8890f699fd2fcad5a109781a922489
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-26 22:08:53 +0530

    Fetching Inventary Details

---
+++ FetchInventoryDetails.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using InventaryMangement;
+using Newtonsoft.Json;
+
+namespace InventoryManagement
+{
+    public class FetchInventoryDetails
+    {
+
+        public Details Read(string Path)
+        {
+            using (StreamReader file = new StreamReader(Path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+
+                    return JsonConvert.DeserializeObject<Details>(json);
+
+                }
+                catch (Exception e)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
+
